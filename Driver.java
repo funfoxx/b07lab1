d018ee5cefb084184bf6e1716518c3776b420886
funfoxx <quanfunfoxjude@gmail.com>
2024-06-02 18:54:47 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Driver {
-	public static void main(String [] args) {
+	public static void main(String [] args) throws FileNotFoundException, IOException{
 		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+		// System.out.println(p.evaluate(3));
+		double [] c1 = {6,5};
+		int [] e1 = {0,3};
+		Polynomial p1 = new Polynomial(c1, e1);
+		System.out.println(p1.evaluate(2));
+		double [] c2 = {-2,-9};
+		int [] e2 = {1,4};
+		Polynomial p2 = new Polynomial(c2, e2);
 		Polynomial s = p1.add(p2);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
 		if(s.hasRoot(1))
 			System.out.println("1 is a root of s");
 		else
 		System.out.println("1 is not a root of s");
+		double [] c3 = {3,1};
+		int [] e3 = {1,2};
+		Polynomial k1 = new Polynomial(c3,e3);
+		double [] c4 = {2,1};
+		int [] e4 = {2,3};
+		Polynomial k2 = new Polynomial(c4, e4);
+		Polynomial l = k1.multiply(k2);
+		System.out.println("l(1) = " + l.evaluate(1));
+		File file = new File("file_read_test.txt");
+		Polynomial o1 = new Polynomial(file);
+		System.out.println("o(2) = " + o1.evaluate(2));
+		double [] c5 = {4,1,3,-5};
+		int [] e5 = {0,2,3,4};
+		Polynomial k5 = new Polynomial(c5,e5);
+		File new_file = k5.saveToFile("file_write_test.txt");
+
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,59 +1,204 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
 public class Polynomial{
 	// field
 	double [] coefficients;
+	int [] exponents;
 
 	// methods
 	public Polynomial(){
-		coefficients = new double[1];
-		coefficients[0] = 0;
+		coefficients = null;
+		exponents = null;
 	}
 
-	public Polynomial(double [] coe){
+	public Polynomial(double [] coe, int [] exp){
 		coefficients = new double[coe.length];
+		exponents = new int[exp.length];
 		for(int i=0; i<coe.length; i++){
 			coefficients[i] = coe[i];
 		}
+		for(int i=0; i<exp.length; i++){
+			exponents[i] = exp[i];
+		}
+	}
+
+	public Polynomial(File file) throws FileNotFoundException{
+		Scanner input = new Scanner(file);
+		String text = input.nextLine();
+		text = text.replace("+", "#");
+		text = text.replace("-", "#-");
+		
+		String [] split_text = text.split("#", -1);
+		double [] final_coe = new double[split_text.length];
+		int [] final_exp = new int[split_text.length];
+		int counter = 0;
+		for(int i=0; i<split_text.length; i++){
+			String split_text_chunk = split_text[i];
+			if(split_text_chunk.indexOf("x") == -1){
+				double coe = Double.parseDouble(split_text_chunk);
+				final_coe[counter] = coe;
+				final_exp[counter] = 0;
+				counter++;
+			}
+			else{
+				String [] split_x = split_text_chunk.split("x", -1);
+				double coe = Double.parseDouble(split_x[0]);
+				int exp = Integer.parseInt(split_x[1]);
+				final_coe[counter] = coe;
+				final_exp[counter] = exp;
+				counter++;
+			}
+		}
+		coefficients = final_coe;
+		exponents = final_exp;
+	}
+
+	public int getIndex(int x, int [] p){
+		for(int i=0; i<p.length; i++){
+			if(p[i] == x) return i;
+		}
+		return -1;
+	}
+
+	public int getIntArrayMax(int [] p){
+		int max = 0;
+		for(int i=0; i<p.length; i++){
+			max = Math.max(p[i], max);
+		}
+		return max;
 	}
 
 	public Polynomial add(Polynomial p){
-		int max_length = Math.max(p.coefficients.length, this.coefficients.length);
-		int min_length = Math.min(p.coefficients.length, this.coefficients.length);
-		int dif = max_length - min_length;
-		double [] sum = new double [max_length];
+		int max_size = Math.max(getIntArrayMax(p.exponents), getIntArrayMax(this.exponents)) + 1;
+		int [] possible_exp = new int[max_size];
+		double [] possible_coe = new double[max_size];
+		for(int i=0; i<possible_exp.length; i++){
+			possible_exp[i] = i;
+		}
 
-		for(int i=0; i<min_length; i++){
-			sum[i] = p.coefficients[i] + this.coefficients[i];
+		// get el in this
+		for(int i=0; i<this.exponents.length; i++){
+			int idx = getIndex(this.exponents[i], possible_exp);
+			possible_coe[idx] += this.coefficients[i];
 		}
-		if(dif == 0){
-			Polynomial new_poly = new Polynomial(sum);
-			return new_poly;
+
+		// get el in other
+		for(int i=0; i<p.exponents.length; i++){
+			int idx = getIndex(p.exponents[i], possible_exp);
+			possible_coe[idx] += p.coefficients[i];
 		}
-		else{
-			if(p.coefficients.length == max_length)
-				for(int j=0; j<dif; j++){
-					sum[min_length + j] = p.coefficients[min_length + j];
-				}
-			else 
-				for(int j=0; j<dif; j++){
-					sum[min_length + j] = this.coefficients[min_length + j];
-				}
+
+		// count how many non zero in possible_coe
+		int size = 0;
+		for(int i=0; i<possible_coe.length; i++){
+			if(possible_coe[i] != 0){
+				size++;
+			}
 		}
-		Polynomial new_poly = new Polynomial(sum);
+
+		// create 
+		double [] final_coe = new double[size];
+		int [] final_exp = new int[size];
+
+		// fill new
+		int final_index = 0;
+		for(int i=0; i<possible_coe.length; i++){
+			if(possible_coe[i] != 0){
+				final_coe[final_index] = possible_coe[i];
+				final_exp[final_index] = possible_exp[i];
+				final_index++;
+			}
+		}
+
+		Polynomial new_poly = new Polynomial(final_coe, final_exp);
 		return new_poly;
 	}
 
 	public double evaluate(double x){
-		int i;
-		double total = 0.0;
-		for(i=0;i<coefficients.length;i++){
-			double pow = i;
-			total += coefficients[i] * Math.pow(x, pow);
+		double sum = 0.0;
+		for(int i=0; i<this.exponents.length; i++){
+			sum += this.coefficients[i] * Math.pow(x, this.exponents[i]);
 		}
-		return total;
+		return sum;
 	}
 
 	public boolean hasRoot(double x){
-		double result = evaluate(x);
+		double result = this.evaluate(x);
 		return (result == 0);
 	}
-} 
+
+	public Polynomial multiply(Polynomial p){
+		int max_size = this.coefficients.length * p.coefficients.length;
+		int step_counter = 0;
+		double [] max_coe = new double[max_size];
+		int [] max_exp = new int[max_size];
+		for(int i=0; i<this.coefficients.length; i++){
+			for(int j=0; j<p.coefficients.length; j++){
+				max_coe[step_counter] = this.coefficients[i] * p.coefficients[j];
+				max_exp[step_counter] = this.exponents[i] + p.exponents[j];
+				step_counter++;
+			}
+		}
+
+		double [] temp_coe = new double[max_size];
+		int [] temp_exp = new int[max_size];
+		for(int i=0; i<temp_exp.length; i++){
+			temp_exp[i] = -1;
+		}
+		int step_counter_2 = -1;
+		for(int i=0; i<max_exp.length; i++){
+			if(getIndex(max_exp[i], temp_exp) == -1){
+				step_counter_2++; 
+				temp_coe[step_counter_2] = max_coe[i];
+				temp_exp[step_counter_2] = max_exp[i];
+			}
+			else{
+				int repeat_index = getIndex(max_exp[i], temp_exp);
+				temp_coe[repeat_index] += max_coe[i];
+			}
+		}
+
+		int length_counter = 0;
+		for(int i=0; i<temp_exp.length; i++){
+			if(temp_exp[i] != -1) length_counter++;
+		}
+
+		double [] final_coe = new double[length_counter];
+		int [] final_exp = new int[length_counter];
+
+		for(int i=0; i<final_exp.length; i++){
+			final_coe[i] = temp_coe[i];
+			final_exp[i] = temp_exp[i];
+		}
+
+		Polynomial final_poly = new Polynomial(final_coe, final_exp);
+		return final_poly;
+	}
+
+	public File saveToFile(String file_name) throws IOException{
+		FileWriter writer = new FileWriter(file_name, false);
+		String final_string = "";
+		for(int i=0; i<this.coefficients.length; i++){
+			if(this.exponents[i] == 0){
+				String temp = this.coefficients[i] + "+";
+				final_string = final_string + temp;
+			}
+			else{
+				String temp = this.coefficients[i] + "x" + this.exponents[i] + "+";
+				final_string = final_string + temp;
+			}
+		}
+		final_string = final_string.replace("+-", "-");
+		final_string = final_string.substring(0, final_string.length() - 1);
+		writer.write(final_string);
+		writer.close();
+
+		File file = new File(file_name);
+		return file;
+	}
+
+}
